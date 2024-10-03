b4572c747693e13978c610aa9c235259fb9930d0
Akshat Sipany <borncancer21@gmail.com>
2024-10-03 20:56:05 +0530

    auth middleware done

--- week-4/middlewares/02-authmiddleware.js
+++ week-4/middlewares/02-authmiddleware.js
@@ -8,7 +8,16 @@ const VALID_API_KEY = '100xdevs_cohort3_super_secret_valid_api_key'; // key is 1
 
 // Middleware to check for a valid API key
 function authenticateAPIKey(req, res, next) {
-    //  authenticate APIKey here
+    const api_key = req.headers['100xdevs-api-key']
+    if(api_key===VALID_API_KEY)
+    {
+        next()
+    }
+    else{
+        res.status(401).json({
+            message : "Invalid or missing API key"
+        })
+    }
 }
 
 app.use(authenticateAPIKey);

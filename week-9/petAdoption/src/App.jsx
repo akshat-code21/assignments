2eaad856bd5dafad8771512378d808b2c728ea63
Akshat Sipany <borncancer21@gmail.com>
2024-11-04 17:45:14 +0530

    commits

--- week-9/petAdoption/src/App.jsx
+++ week-9/petAdoption/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
@@ -63,25 +62,6 @@ function App() {
         )}
       </div>
     </>
-=======
-import Header from './components/Header';
-import PetAdoptionForm from './components/PetAdoptionForm';
-import "./myApp.css";
-
-
-const App = () => {
-  return (
-    <div
-      style={{
-        backgroundImage: "url('https://images.unsplash.com/photo-1450778869180-41d0601e046e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1950&q=80')",
-        height: "100vh",
-        backgroundSize: "cover"
-      }}
-    >
-      <Header message={"Pet Adoption Form"} />
-      <PetAdoptionForm />
-    </div>
->>>>>>> ac02966658ccf0427a1abb6dc38aab64af95fa0c
   );
 };
 export default App;

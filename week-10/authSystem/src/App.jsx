51f3ec18331fe773616274f227d7ddaccca16f80
Akshat Sipany <borncancer21@gmail.com>
2024-11-05 17:06:47 +0530

    authSystem using ContextAPI left

--- week-10/authSystem/src/App.jsx
+++ week-10/authSystem/src/App.jsx
@@ -7,9 +7,35 @@ import { useState } from 'react';
 function App() {
   const [isLogin,setIsLogin] = useState(false)
   const [name,setName] = useState("")
+  const handleSwitch = () =>{
+    // console.log('hi');
+    setIsLogin(false)
+    
+  }
   return (
     <div>
       <AppBar isLogin={isLogin} setIsLogin={setIsLogin} name={name} setName={setName}></AppBar>
+      <div style={{
+        height : 50,
+        width : '100vw',
+        backgroundColor : 'aqua',
+        display : "flex",
+        alignItems : "center",
+        position : 'relative',
+      }}>
+        <button style={{
+          position : "absolute",
+          right : 10,
+          backgroundColor: "#3f98b5",
+          color: "white",
+          border: "none",
+          padding: "0.5rem 1rem",
+          borderRadius: 4,
+          cursor: "pointer",
+        }} onClick={handleSwitch}>
+          Switch to ContextAPI
+        </button>
+      </div>
       {isLogin ? <Home></Home> : <Login setIsLogin={setIsLogin} setName={setName}></Login>}
     </div>
   );
